935436315a7387a843193d17dc9f985d26d8a375
syedumars <syed.umar.s@volvocars.com>
2022-08-06 15:57:38 +0200

    adding my JUnit test files

---
+++ src/main/java/test/com/programming/ContactManagerTest.java
@@ -0,0 +1,19 @@
+package test.com.programming;
+
+import com.programming.techie.ContactManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class ContactManagerTest {
+
+    @Test
+    public void shouldCreateContact() {
+        ContactManager contactManager = new ContactManager();
+        contactManager.addContact("Syed", "Umar", "0739796285");
+
+        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        Assertions.assertEquals(1,contactManager.getAllContacts().size());
+    }
+}
