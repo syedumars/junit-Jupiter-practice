32b7035d16c3e7cbb12e5d70643c15e68f4bf0da
syedumars <syed.umar.s@volvocars.com>
2022-08-07 12:34:23 +0200

    learnt the Ordering of tests

--- src/main/java/test/com/programming/ContactManagerTest.java
+++ src/main/java/test/com/programming/ContactManagerTest.java
@@ -1,19 +1,70 @@
 package test.com.programming;
 
 import com.programming.techie.ContactManager;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+
 
 //import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ContactManagerTest {
+    ContactManager contactManager;
+
+    @BeforeAll
+    public  void setupAll(){
+        System.out.println("Before All Tests, declaration happens...");
+        System.out.println("Current OS we are using is : " + System.getProperty("os.name"));
+    }
+
+    @BeforeEach
+    @DisplayName("Before each test cases, initialization happens...")
+    public void setup_each(){
+        System.out.println("Before each test cases, initialization happens...");
+        contactManager = new ContactManager();
+    }
 
     @Test
+    //@EnabledOnOs(value = OS.MAC, disabledReason = "Enabled only on MAC ")
+   //  @EnabledOnOs(value = OS.WINDOWS, disabledReason = "Enabled only on WINDOWS ")
+   // @DisplayName("Adding a contact, Enabled only on particular OS")
     public void shouldCreateContact() {
-        ContactManager contactManager = new ContactManager();
-        contactManager.addContact("Syed", "Umar", "0123456789");
+        System.out.println("TEST Method 1 start ");
+       // System.out.println("Windows method");
+       // contactManager.addContact("Syed", "Umar", "0123456789");
+        //   Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        //  Assertions.assertEquals(1,contactManager.getAllContacts().size());
+        //  System.out.println(" @Test executed, check if contact created");
+        System.out.println("TEST Method 1 end");
+    }
 
-        Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
-        Assertions.assertEquals(1,contactManager.getAllContacts().size());
+
+    @Test
+    //@EnabledOnOs(value = OS.MAC, disabledReason = "Enabled only on MAC ")
+    public void randomTest() {
+        System.out.println("TEST Method 2 start");
+      //  contactManager.addContact("Syed", "Umar", "0123456789");
+        // Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
+        // Assertions.assertEquals(1,contactManager.getAllContacts().size());
+        // System.out.println(" @Test executed, check if contact created");
+        System.out.println("TEST Method 2 end");
+    }
+
+    @AfterEach
+    @DisplayName("After each test cases executed")
+    public void AfterEach(){
+        System.out.println("After Each method called...");
+    }
+
+
+
+
+    @AfterAll
+     public void AfterAllTest(){
+        System.out.println("After All method called...");
+        Assertions.assertThrows(RuntimeException.class, () -> contactManager.addContact("Shaa","Shu",""));
     }
 }
